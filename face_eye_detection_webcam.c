f2558bec709c997eb38390d7203e6860103651dc
sushithbalu <sbalu1132@gmail.com>
2016-12-20 02:26:11 +0530

    eye detection

--- face_eye_detection_webcam.c
+++ face_eye_detection_webcam.c
@@ -8,47 +8,52 @@ status:Incomplete
 TODO:cpu usage issue, code cleanup
  */
 
-typedef void (*callback_prototype)(IplImage *, CvHaarClassifierCascade *, 
+typedef IplImage* (*callback_prototype)(IplImage*, CvHaarClassifierCascade *, 
 		CvMemStorage *);
 
 //to locate cascade use: locate haarcascade_eye.xml
 const char* eye_cascade_name = "/usr/share/opencv/haarcascades/haarcascade_eye.xml";
 
-char *window_name = "result";
 
-void detect_eye(IplImage* frame, CvHaarClassifierCascade *cascade, 
+IplImage* detect_eye(IplImage* frame, CvHaarClassifierCascade *cascade, 
 		CvMemStorage * storage)
 {
 	cvClearMemStorage(storage);
-	
 
-	CvSeq *eyes = cvHaarDetectObjects(frame, cascade, storage, 1.1, 3,
-			CV_HAAR_DO_CANNY_PRUNING, cvSize(10, 10), cvSize(30, 30));
+	IplImage * gray_frame = 0;
+	gray_frame = cvCreateImage(cvSize(frame->width, frame->height), frame->depth, 1);
+	if (!gray_frame)
+	{
+		fprintf(stderr, "cvCreateImage failed!\n" );
+		return NULL;
+	}
+
+	cvCvtColor(frame, gray_frame, CV_RGB2GRAY);
+
+	CvSeq *eyes = cvHaarDetectObjects(gray_frame, cascade, storage, 1.1, 2,
+			CV_HAAR_DO_CANNY_PRUNING, cvSize(20, 20), cvSize(0, 0));
 	CvPoint cvp;
-
 	int LINE_TYPE = CV_AA;
-	CvFont font;
-	cvInitFont(&font, CV_FONT_HERSHEY_PLAIN, 1.0f, 1.0f, 0.0f, 1, CV_AA);
 
 	for(int i = 0; i < (eyes ? eyes->total: 0); i++) {
 
 		CvRect *r  = (CvRect *)cvGetSeqElem(eyes, i);
 
-		cvp.x = r->x + cvRound(r->width/2);
-		cvp.y = r->y + cvRound(r->height/2);
+		cvp.x = r->x + r->width/2;
+		cvp.y = r->y + r->height/2;
 
-		cvCircle(frame, cvp, cvRound(r->width/2), CV_RGB(255,0,0), 1,
+		cvCircle(gray_frame, cvp, r->width/2, CV_RGB(255,0,0), 1,
 				LINE_TYPE, 0 );
-		cvPutText(frame, "open", cvPoint(10, 20), &font, CV_RGB(255,0,0));
-		//cvCircle(frame, cvp, 1, CV_RGB(255,0,0), 1, LINE_TYPE, 0 );
+		cvCircle(gray_frame, cvp, 1, CV_RGB(255,0,0), 1, LINE_TYPE, 0 );
 	}
 
-	cvShowImage( window_name, frame );
+	return gray_frame;
 }
 
-void process_video(
+void process_video(callback_prototype custom_callback, 
 		CvHaarClassifierCascade *cascade, CvMemStorage *storage)
 {           
+	char *window_name = "result";
 
 	//initialize cascade
 	cascade = (CvHaarClassifierCascade *)cvLoad(eye_cascade_name, 0, 0, 0);
@@ -59,9 +64,8 @@ void process_video(
 
 	// Initialize camera
 	CvCapture *capture = 0;
-	
-	// , set -1 for first available cam
-	capture = cvCaptureFromCAM(-1);
+
+	capture = cvCaptureFromCAM(0);
 	if (!capture) 
 	{
 		fprintf(stderr, "Error: webcam initialization failed\n" );
@@ -69,9 +73,8 @@ void process_video(
 	}
 	
 	// set frame resolution	
-//	cvSetCaptureProperty(capture,CV_CAP_PROP_FRAME_WIDTH, 640);
-//	cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT, 480);
-//	cvSetCaptureProperty(capture, CV_CAP_PROP_FPS , 10);
+	cvSetCaptureProperty(capture,CV_CAP_PROP_FRAME_WIDTH, 320);
+	cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT, 240);
 
 	// Create a window for the video 
 	cvNamedWindow(window_name, CV_WINDOW_AUTOSIZE);
@@ -87,10 +90,10 @@ void process_video(
 			break;
 		}
 
-		detect_eye(frame,cascade ,storage);
+		IplImage* processed_frame = (*custom_callback)(frame,cascade ,storage);
 
-		//cvShowImage(window_name, processed_frame);
-		//cvReleaseImage(&processed_frame);
+		cvShowImage(window_name, processed_frame);
+		cvReleaseImage(&processed_frame);
 		key = cvWaitKey(10);
 	}
 
@@ -98,6 +101,7 @@ void process_video(
 		cvReleaseMemStorage(&storage);
 	if(cascade)
 		cvReleaseHaarClassifierCascade(&cascade);
+
 
 	cvDestroyWindow(window_name);
 	cvReleaseCapture(&capture);
@@ -108,7 +112,7 @@ int main( int argc, char **argv )
 	CvHaarClassifierCascade * cascade = 0;
 	CvMemStorage * storage = 0;
 
-	process_video(cascade, storage);
+	process_video(detect_eye, cascade, storage);
 
 	return 0;
 }
